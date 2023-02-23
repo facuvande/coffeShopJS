da119d0cca53d5056ade96edecdc02a256077f1f
Facundo Vandecaveye <facundovs84@hotmail.com.ar>
2023-02-23 14:03:38 -0300

    Se agrego menu responsive

--- js/index.js
+++ js/index.js
@@ -3,3 +3,18 @@ const $toggle = document.getElementById('theme-toggle');
 $toggle.addEventListener("click", (e) =>{
     $toggle.classList.toggle("theme-toggle--toggled")
 })
+
+const $panelBtn = document.querySelector('.panel-btn');
+const $aside = document.querySelector('.panel')
+
+document.addEventListener("click", (e) =>{
+    if(e.target.matches('.panel-btn') || e.target.matches('.panel-btn *')){
+        $panelBtn.classList.toggle("is-active")
+        $aside.classList.toggle("is-active")
+    }
+
+    if(e.target.matches('#linkBtn')){
+        $panelBtn.classList.remove("is-active")
+        $aside.classList.remove("is-active")
+    }
+})
